e05864f2265b0df4e6b264396e9d0a27d52900c2
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-03-15 18:34:03 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1136 ms, Memory: 86432 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -1,8 +1,1 @@
-num = int(input())
-r = []
-for i in range(num) :
-    a = int(input())
-    r.append(a)
-r.sort(reverse=True)
-for i in r :
-    print(i) 
+print(*sorted(list(map(int,open(0)))[1:],reverse=1))
