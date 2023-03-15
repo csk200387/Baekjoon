957aef4c5ad1ad80519f75fe20fac70aaec73352
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-03-15 18:34:45 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9536 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,9 +1,9 @@
 import sys
-input = lambda:sys.stdin.readline().rstrip()
-ar = [0]*10001
-for i in range(int(input())) :
-    ar[int(input())-1] += 1
-for i in range(10001) :
+input = lambda:int(sys.stdin.readline().rstrip())
+ar = [0]*10000
+for i in range(input()) :
+    ar[input()-1] += 1
+for i in range(10000) :
     if ar[i] > 0 :
         for _ in range(ar[i]) :
             print(i+1)
