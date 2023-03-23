d5bec27fd7306803764a8873bdd14e9256a53115
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-03-23 19:49:58 +0900

    [Bronze V] Title: 나머지, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.py
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -1,7 +1,4 @@
-inputNum = input("").split(" ")
-A = int(inputNum[0])
-B = int(inputNum[1])
-C = int(inputNum[2])
+A, B, C = map(int, input().split())
 print((A+B)%C)
 print(((A%C) + (B%C))%C)
 print((A*B)%C)
