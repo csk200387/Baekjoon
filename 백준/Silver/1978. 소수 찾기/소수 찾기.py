79cdeb2d76c8e825648ab2128c43d6b7b3a441ca
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-03-29 22:39:17 +0900

    [Silver V] Title: 소수 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,11 +1,10 @@
-aa = input()
-inp = list(map(int,input().split()))
-res = []
-for a in inp :
-    e = 0
-    for i in range(2,a+1):
-        if a % i == 0 :
-            e += 1
-        if i == a and e == 1:
-            res.append(a)
-print(len(res))
+input()
+r = 0
+for a in map(int,input().split()) :
+  e = 0
+  for i in range(2,a+1):
+    if a % i == 0 :
+      e += 1
+      if i == a and e == 1:
+        r += 1
+print(r)
