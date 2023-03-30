be7fbe9167fd36a5563b06c355922d2e7d4fae8e
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-03-30 18:34:47 +0900

    [Bronze IV] Title: 과자, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10156. 과자/과자.py
+++ 백준/Bronze/10156. 과자/과자.py
@@ -1,5 +1,2 @@
-n = list(map(int,input().split()))
-if (n[0]*n[1])-n[2] > 0:
-    print((n[0]*n[1])-n[2])
-else :
-    print(0)
+a,b,c = map(int,input().split())
+print((a*b)-c if (a*b)-c > 0 else 0)
