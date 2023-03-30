9217c9b7c6b32fd1083d3e2852593462cb0d6060
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-03-30 23:39:59 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 56 ms, Memory: 47348 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,25 +1,8 @@
+import math
 char = """***
 * *
 ***"""
-num = int(input())
-def tlqkf(num) :
-    if num == 3 :
-        return 1
-    elif num == 9 :
-        return 2
-    elif num == 27 :
-        return 3
-    elif num == 81 :
-        return 4
-    elif num == 243 :
-        return 5
-    elif num == 729 :
-        return 6
-    elif num == 2187 :
-        return 7
-    else :
-        return 8
-num = tlqkf(num)
+num = round(math.log(int(input()), 3))
 if num == 1 :
     print(char)
 else :
