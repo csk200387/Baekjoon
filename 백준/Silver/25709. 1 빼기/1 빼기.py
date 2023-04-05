3bc6914b9b290c1dd7b74c37dcf383a52fe01ac8
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-05 20:36:54 +0900

    [Silver III] Title: 1 빼기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/25709. 1 빼기/1 빼기.py
+++ 백준/Silver/25709. 1 빼기/1 빼기.py
@@ -1,16 +1,9 @@
-def removeone(n) :
-    n = str(n)
-    if n.count("1") == 0 :
-        return int(n)
-    n = n[:n.index("1")] + n[n.index("1")+1:]
-    return int(n)
-
-n = int(input())
-count = 0
-while n != 0 :
-    if n-1 == 0 :
-        count += 1
-        break
-    n = min(n-1, removeone(n))
-    count += 1
-print(count)
+n=int(input())
+c=0
+while n!=0:
+ if n-1==0:
+  c+=1
+  break
+ n=min(n-1,int(str(n).replace("1","",1)))
+ c+=1
+print(c)
