8ba73ee23ef0302a4cfdabb842ffb72a36fec2b4
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-08 00:05:57 +0900

    [Bronze III] Title: 알람 시계, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,11 +1,7 @@
-num = input("").split(" ")
-A = int(num[0])
-B = int(num[1])
+A, B = map(int, input().split())
 C = B - 45
-
 if(C < 0):
     A -= 1
-    if(A < 0):
-        A = 23
-    C = 60 + C
+    if(A < 0): A = 23
+    C += 60
 print(A,C)
