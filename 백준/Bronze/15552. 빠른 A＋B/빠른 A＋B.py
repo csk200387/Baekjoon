904253d3bb287aa95f9108aabb85d4befa9953e1
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-10 17:36:19 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1188 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -1,5 +1,6 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
+print=sys.stdout.write;
 for _ in range(int(input())):
     a,b=map(int,input().split())
-    print(a+b)
+    print(f"{a+b}\n")
