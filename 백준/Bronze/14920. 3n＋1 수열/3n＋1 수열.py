ff11c8690ca248f5245a111f14d3232c7fcc7b09
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-10 17:37:43 +0900

    [Bronze III] Title: 3n+1 수열, Time: 44 ms, Memory: 31268 KB -BaekjoonHub

--- 백준/Bronze/14920. 3n＋1 수열/3n＋1 수열.py
+++ 백준/Bronze/14920. 3n＋1 수열/3n＋1 수열.py
@@ -1,13 +1,9 @@
-i = 1
-def cfun(n) :
-    global i
+def cfun(n, i=1) :
     if n == 1 :
         return i
     elif n%2 == 0 :
-        i += 1
-        return cfun(n//2)
+        return cfun(n//2, i+1)
     else :
-        i += 1
-        return cfun(3*n+1)
+        return cfun(3*n+1, i+1)
 num = int(input())
 print(cfun(num))
