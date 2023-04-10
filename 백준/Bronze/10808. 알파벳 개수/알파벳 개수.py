0d7e7af71a9916a5b69bab1d5137dac80a666b5f
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-10 17:37:47 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -1,7 +1,4 @@
-al = list("abcdefghijklmnopqrstuvwxyz")
-b = [0]*26
-a = input()
-for i in range(len(a)-1,-1,-1) :
-    b[al.index(a[i])] += 1
-for i in b :
-    print(i)
+t = input()
+for i in range(26) :
+  tmp = t.count(chr(97+i))
+  print(tmp, end=" ")
