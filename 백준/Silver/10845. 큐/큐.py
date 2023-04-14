f92789be664a7c48c02ff50cdda69e1f4b895b5c
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-15 00:57:08 +0900

    [Silver IV] Title: 큐, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,49 +1,64 @@
 import sys
 input = lambda:sys.stdin.readline().rstrip()
-ar = []
-def push(n) :
-    ar.append(n)
 
-def pop() :
-    if len(ar) == 0 :
-        print(-1)
-    else :
-        print(ar.pop(0))
+class queue :
 
-def size() :
-    print(len(ar))
+    def __init__(self) :
+        """큐 생성자"""
+        self.ar = []
 
-def empty() :
-    if len(ar) == 0 :
-        print(1)
-    else :
-        print(0)
+    def push(self, n) :
+        """큐에 n을 넣는다."""
+        self.ar.append(n)
 
-def front() :
-    t = len(ar)
-    if t == 0 :
-        print(-1)
-    else :
-        print(ar[0])
+    def pop(self) :
+        """큐에서 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다."""
+        if len(self.ar) == 0 :
+            print(-1)
+        else :
+            print(self.ar.pop(0))
 
-def back() :
-    t = len(ar)
-    if t == 0 :
-        print(-1)
-    else :
-        print(ar[len(ar)-1])
+    def size(self) :
+        """큐에 들어있는 정수의 개수를 출력한다."""
+        print(len(self.ar))
+
+    def empty(self) :
+        """큐가 비어있으면 1, 아니면 0을 출력한다."""
+        if len(self.ar) == 0 :
+            print(1)
+        else :
+            print(0)
+
+    def front(self) :
+        """큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다."""
+        t = len(self.ar)
+        if t == 0 :
+            print(-1)
+        else :
+            print(self.ar[0])
+
+    def back(self) :
+        """큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다."""
+        t = len(self.ar)
+        if t == 0 :
+            print(-1)
+        else :
+            print(self.ar[len(self.ar)-1])
+
+queue = queue() #큐 생성
+
 if __name__ == "__main__" :
     for _ in range(int(input())) :
         tmp = input().split()
         if tmp[0] == "push" :
-            push(int(tmp[1]))
+            queue.push(int(tmp[1]))
         elif tmp[0] == "pop" :
-            pop()
+            queue.pop()
         elif tmp[0] == "size" :
-            size()
+            queue.size()
         elif tmp[0] == "empty" :
-            empty()
+            queue.empty()
         elif tmp[0] == "front":
-            front()
+            queue.front()
         else :
-            back()
+            queue.back()
