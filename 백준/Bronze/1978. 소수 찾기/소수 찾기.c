57ddfb829d3bebe38161d5a02e1d47a92c98d126
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-15 19:06:23 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.c
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
 int main() {
-	int a[101];
-	int n, c = 0;
+	int t, n, c = 0;
 	if (scanf("%d", &n) != 1) return 0;
 
 	for (int i = 0; i < n; i++) {
-		if (scanf("%d", &a[i]) != 1) return 0;
-		// find prime number
-		if (a[i] == 2) c++;
-		else if (a[i] > 2) {
+		if (scanf("%d", &t) != 1) return 0;
+		if (t == 2) c++;
+		else if (t > 2) {
 			int j = 2;
-			while (j < a[i]) {
-				if (a[i] % j == 0) break;
+			while (j < t) {
+				if (t % j == 0) break;
 				j++;
 			}
-			if (j == a[i]) c++;
+			if (j == t) c++;
 		}
 	}
 	printf("%d\n", c);
