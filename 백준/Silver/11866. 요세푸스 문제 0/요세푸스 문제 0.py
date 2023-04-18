015989cce8280f6e0d5f0d6053f3486bf23723b7
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-18 18:53:47 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 120 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,9 +1,9 @@
-num = list(map(int,input().split()))
-arr = list(range(1,num[0]+1))
+a,b = map(int, input().split())
+arr = list(range(1,a+1))
 res = []
-for i in range(num[0]) :
-    for l in range(num[1]) :
-        if l == num[1]-1 :
+for i in range(a) :
+    for l in range(b) :
+        if l == b-1 :
             res.append(arr.pop(0))
         else :
             arr.append(arr.pop(0))
