be4e81a7225e6759978ed7a4e51b3380c3b59b93
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-22 01:44:00 +0900

    [Gold IV] Title: 전화번호 목록, Time: 180 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,47 +1,14 @@
 import sys
 input = lambda:sys.stdin.readline().rstrip()
 
-class Node :
-    def __init__(self, key) :
-        self.key = key
-        self.children = {}
-        self.end = False
-        
-class Trie :
-    def __init__(self) :
-        self.root = Node(None)
-        
-    def insert(self, string) :
-        cur = self.root
-        for char in string :
-            if char not in cur.children :
-                cur.children[char] = Node(char)
-            cur = cur.children[char]
-        cur.end = True
-        
-    def search(self, string) :
-        cur = self.root
-        for char in string :
-            if char not in cur.children :
-                return False
-            cur = cur.children[char]
-            if cur.end :
-                return True
-        return False
-    
 t = int(input())
-for _ in range(t) :
+for i in range(t):
     n = int(input())
-    arr = [input().strip() for _ in range(n)]
-    arr.sort()
-    trie = Trie()
-    flag = False
-    for i in arr :
-        if trie.search(i) :
-            flag = True
+    s = [input() for j in range(n)]
+    s.sort()
+    for j in range(1, n):
+        if s[j-1] == s[j][:len(s[j-1])]:
+            print("NO")
             break
-        trie.insert(i)
-    if flag :
-        print("NO")
-    else :
+    else:
         print("YES")
