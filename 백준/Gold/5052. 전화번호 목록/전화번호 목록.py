10027d1372f19e97b457eb2c942efec745988c4f
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-22 01:46:31 +0900

    [Gold IV] Title: 전화번호 목록, Time: 300 ms, Memory: 124700 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,14 +1,8 @@
-import sys
-input = lambda:sys.stdin.readline().rstrip()
-
-t = int(input())
-for i in range(t):
-    n = int(input())
-    s = [input() for j in range(n)]
-    s.sort()
-    for j in range(1, n):
-        if s[j-1] == s[j][:len(s[j-1])]:
-            print("NO")
-            break
-    else:
-        print("YES")
+for i in range(int(input())):
+ s=[input()for j in range(int(input()))]
+ s.sort()
+ for j in range(1,len(s)):
+  if s[j-1]==s[j][:len(s[j-1])]:
+   print("NO")
+   break
+ else:print("YES")
