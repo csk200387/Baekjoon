b32fd103b1ac261424290294a726581b516fb1d7
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-22 13:19:19 +0900

    [Bronze III] Title: 상금 헌터, Time: 124 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/15953. 상금 헌터/상금 헌터.py
+++ 백준/Bronze/15953. 상금 헌터/상금 헌터.py
@@ -1,36 +1,14 @@
+A = [5000000] + [3000000]*2 + [2000000]*3 + [500000]*4 + [300000]*5 + [100000]*6
+B = [5120000] + [2560000]*2 + [1280000]*4 + [640000]*8 + [320000]*16
 for i in range(int(input())) :
     a, b = map(int, input().split())
     price = 0
-    if a == 0 :
-        price = 0
-    elif a == 1 :
-        price = 5000000
-    elif a <= 3 :
-        price = 3000000
-    elif a <= 6 :
-        price = 2000000
-    elif a <= 10 :
-        price = 500000
-    elif a <= 15 :
-        price = 300000
-    elif a <= 21 :
-        price = 100000
-    else :
-        price = 0
-
-    if b == 0 :
-        price += 0
-    elif b == 1 :
-        price += 5120000
-    elif b <= 3 :
-        price += 2560000
-    elif b <= 7 :
-        price += 1280000
-    elif b <= 15 :
-        price += 640000
-    elif b <= 31 :
-        price += 320000
+    if 0 < a <= 21 :
+        price += A[a-1]
     else :
         price += 0
-
+    if 0 < b <= 31 :
+        price += B[b-1]
+    else :
+        price += 0
     print(price)
