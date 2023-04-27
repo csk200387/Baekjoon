e41911abeeede86c468306103424e5ee5aca41b3
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-27 14:51:06 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,6 +1,7 @@
-res = 0
-tmp = 1
-for i in range(int(input())) :
-    res += tmp
-    tmp = res - tmp
-print(res)
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+n = int(input())
+print(fibonacci(n))
