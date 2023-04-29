f40b1342b755b8f9dd7c28896c736a41f49ecf1a
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-29 16:26:05 +0900

    [Bronze II] Title: 숫자의 개수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,11 +1,2 @@
-A = int(input())
-B = int(input())
-C = int(input())
-sum = A*B*C
-dic = {}
-for i in range(10) :
-    dic[str(i)] = 0
-for i in str(sum) :
-    dic[i] += 1
-for i in dic.values() :
-    print(i)
+a,b,c=map(int,open(0))
+for i in range(10):print(str(a*b*c).count(str(i)))
