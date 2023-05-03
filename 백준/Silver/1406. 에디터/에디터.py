46be137d216b1b13becda91409cdbc2bd37c715d
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-05-04 01:39:41 +0900

    [Silver II] Title: 에디터, Time: 196 ms, Memory: 77492 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,21 +1,11 @@
-import sys
-input = lambda:sys.stdin.readline().rstrip()
-
-rStack = [*input()]
-lStack = []
-
-for _ in range(int(input())):
-    cmd = input()
-    if cmd == 'L':
-        if rStack:
-            lStack.append(rStack.pop())
-    elif cmd == 'D':
-        if lStack:
-            rStack.append(lStack.pop())
-    elif cmd == 'B':
-        if rStack:
-            rStack.pop()
-    else:
-        rStack.append(cmd[2])
-
-print(''.join(rStack + lStack[::-1]))
+r,_,*C=open(0)
+r,l=[*r[:-1]],[]
+for c in C:
+ if c=='L\n':
+  if r:l.append(r.pop())
+ elif c == 'D\n':
+  if l:r.append(l.pop())
+ elif c == 'B\n':
+  if r:r.pop()
+ else:r.append(c[2])
+print(''.join(r + l[::-1]))
