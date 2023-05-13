447d3ec40b00bb82f61bcf8d04395f9d0de41eca
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-05-13 19:50:17 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 68 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,23 +1,15 @@
 from collections import Counter
-
-string = input()
-count = Counter(string)
-count = sorted(count.items(), key=lambda x: x[0])
-tmp = 0
-for i in count:
-    if i[1] % 2 != 0:
-        tmp += 1
-if tmp > 1:
-    print("I'm Sorry Hansoo")
-    exit(0)
-
-st, t = "", ""
-if len(string) % 2 == 0:
-    for x, y in count:
-        st += x * (y // 2)
-else :
-    for x, y in count:
-        if y % 2 != 0:
-            t = x
-        st += x * (y // 2)
-print(st + t + st[::-1])
+C=Counter(input())
+C=sorted(C.items(), key=lambda x: x[0])
+t=0
+for i in C:
+ if i[1]%2!=0:
+  t+=1
+  if t>1:
+   print("I'm Sorry Hansoo")
+   exit(0)
+s,t="",""
+for x,y in C:
+ if y%2!=0:t=x
+ s+=x*(y//2)
+print(s+t+s[::-1])
