c7897e0a3de5549fc3e21df265ef1063df935174
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-05-15 19:18:37 +0900

    [Gold I] Title: 이항 계수 3, Time: 760 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.py
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.py
@@ -1,24 +1,20 @@
-MOD = 1000000007
+M = 1000000007
 def factorial(n):
     result = 1
     for i in range(1, n + 1):
-        result = (result * i) % MOD
+        result = (result * i) % M
     return result
-
 def power(n, k):
     if k == 0:
         return 1
     elif k == 1:
-        return n % MOD
+        return n % M
     elif k % 2 == 0:
         y = power(n, k // 2)
-        return (y * y) % MOD
+        return (y * y) % M
     else:
-        return (n * power(n, k - 1)) % MOD
-
+        return (n * power(n, k - 1)) % M
 n, k = map(int, input().split())
 N = factorial(n)
-K = factorial(n-k) * factorial(k) % MOD
-
-
-print(N*power(K, MOD-2)%MOD)
+K = factorial(n-k) * factorial(k) % M
+print(N*power(K, M-2)%M)
