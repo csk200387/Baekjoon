2c60dfec49464bb19aa0324146d1e3e2ab2651a2
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-06-04 17:53:27 +0900

    [Silver V] Title: Identify, Sort, Index, Solve, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/26150. Identify， Sort， Index， Solve/Identify， Sort， Index， Solve.py
+++ 백준/Silver/26150. Identify， Sort， Index， Solve/Identify， Sort， Index， Solve.py
@@ -6,6 +6,4 @@ for _ in range(int(input())):
     arr.append((title, int(num), int(diff)))
 arr.sort(key=lambda x: (-x[1], x[2], x[0]), reverse=True)
 for i in arr:
-    t = i[0]
-    d = i[2]
-    print((t[d-1]).upper(), end="")
+    print((i[0][i[2]-1]).upper(), end="")
