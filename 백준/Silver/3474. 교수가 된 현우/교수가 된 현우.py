7a3e6b7ad6f08ca44e104297bad8a865c518c250
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-06-09 14:06:05 +0900

    [Silver III] Title: 교수가 된 현우, Time: 224 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.py
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.py
@@ -1,15 +1,10 @@
 import sys
 input = lambda:sys.stdin.readline().rstrip()
-
 def count_factors_zero2(n):
     count = 0
     for i in range(1, 13):
         count += n // (5 ** i)
     return count
-
-
 n = int(input())
-
 for i in range(n) :
-    t = int(input())
-    print(count_factors_zero2(t))
+    print(count_factors_zero2(int(input())))
