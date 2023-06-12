e52d2917ae0da6ce273d5f5366e0ce0fbbb3fdd4
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-06-12 17:18:39 +0900

    [Bronze II] Title: 주사위 네개, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2484. 주사위 네개/주사위 네개.py
+++ 백준/Bronze/2484. 주사위 네개/주사위 네개.py
@@ -1,21 +1,12 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
-n = int(input())
-result = []
-
-for i in range(n):
-    ip = list(map(int, input().split()))
-    c = max(ip, key=ip.count)
-    m = ip.count(c)
-    if m == 4:
-        result.append(50_000 + c * 5_000)
-    elif m == 3:
-        result.append(10_000 + c * 1_000)
-    elif m == 2 and len(set(ip)) == 2:
-        result.append(2_000 + sum(set(ip)) * 500)
-    elif m == 2:
-        result.append(1_000 + c * 100)
-    else:
-        result.append(max(ip) * 100)
-print(max(result))
+r=[]
+a=lambda n:r.append(n)
+for i in [*open(0)][1:]:
+ ip=[*map(int, i.split())]
+ c=max(ip,key=ip.count)
+ m=ip.count(c)
+ if m==4:a(50000+c*5000)
+ elif m==3:a(10000+c*1000)
+ elif m==2 and len(set(ip))==2:a(2000+sum(set(ip))*500)
+ elif m==2:a(1000+c*100)
+ else:a(max(ip)*100)
+print(max(r))
