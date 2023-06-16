fa5200525e44b9b2561b98687da8803634af6f5e
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-06-16 22:18:37 +0900

    [Silver II] Title: 늑대와 올바른 단어, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/13022. 늑대와 올바른 단어/늑대와 올바른 단어.py
+++ 백준/Silver/13022. 늑대와 올바른 단어/늑대와 올바른 단어.py
@@ -1,3 +1,3 @@
 s=input()
-for i in range(s.count("w"),0,-1):s=s.replace("w"*i+"o"*i+"l"*i+"f"*i,"")
-print(~int(bool(s))+2)
+for i in range(13,0,-1):s=s.replace("w"*i+"o"*i+"l"*i+"f"*i,"")
+print(0 if s else 1)
