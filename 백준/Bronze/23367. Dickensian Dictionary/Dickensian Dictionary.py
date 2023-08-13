e19624d2ec3f04893c52384b9337cf3dfd076bc9
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-08-13 12:30:43 +0900

    [Bronze II] Title: Dickensian Dictionary, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/23367. Dickensian Dictionary/Dickensian Dictionary.py
+++ 백준/Bronze/23367. Dickensian Dictionary/Dickensian Dictionary.py
@@ -1,9 +1,9 @@
-r = "yuiophjklnm"
-word = input()
-s = [word[0] in r]
-l = "yes"
-for i in range(1, len(word)):
-    s += [word[i] in r]
-    if s[-1] == s[-2]:
-        l = "no"
-print(l)
+r="yuiophjklnm"
+w=input()
+s=[w[0] in r]
+for i in range(1, len(w)):
+ s+=[w[i] in r]
+ if s[-1]==s[-2]:
+  print("no")
+  exit()
+print("yes")
