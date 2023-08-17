26d80e8dea08090e182a260b694aa203c987e70f
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-08-17 12:15:18 +0900

    [Bronze I] Title: FizzBuzz, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -1,16 +1,11 @@
-arr = list(map(str.strip, open(0)))
-n,idx = 0, 0
+a=list(map(str.strip, open(0)))
+n,d=0,0
 for i in range(3):
-    if arr[i].isdigit():
-        n = int(arr[i])
-        idx = i
-        break
-res = n+3-idx
-if res%3 == 0 and res%5 == 0:
-    print("FizzBuzz")
-elif res%3 == 0:
-    print("Fizz")
-elif res%5 == 0:
-    print("Buzz")
-else:
-    print(str(res))
+ if a[i].isdigit():
+  n,d=int(a[i]),i
+  break
+r=n+3-d
+if r%3==0 and r%5==0:print("FizzBuzz")
+elif r%3==0:print("Fizz")
+elif r%5==0:print("Buzz")
+else:print(r)
