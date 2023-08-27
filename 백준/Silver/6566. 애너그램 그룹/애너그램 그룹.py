e52c75b139d6d537b301419d2153a78fc5edbd63
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-08-27 16:10:43 +0900

    [Silver I] Title: 애너그램 그룹, Time: 76 ms, Memory: 35552 KB -BaekjoonHub

--- 백준/Silver/6566. 애너그램 그룹/애너그램 그룹.py
+++ 백준/Silver/6566. 애너그램 그룹/애너그램 그룹.py
@@ -1,11 +1,11 @@
-import sys
-from collections import defaultdict
-input = lambda:sys.stdin.readline().rstrip()
-dict = defaultdict(list)
-for line in sys.stdin:
+dict = {}
+for line in [*open(0)]:
     data = line.rstrip()
     s_data = "".join(sorted(data))
-    dict[s_data].append(data)
+    if s_data not in dict:
+        dict[s_data] = [data]
+    else:
+        dict[s_data].append(data)
 key_list = sorted(dict.keys(), key=lambda x: (-len(dict[x]), dict[x]))
 for i in range(5):
     new_list = sorted(set(dict[key_list[i]]))
