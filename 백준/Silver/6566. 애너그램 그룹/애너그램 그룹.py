d39e3cdf9b36ca73a824dace4871334c512e74c8
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-08-27 16:14:26 +0900

    [Silver I] Title: 애너그램 그룹, Time: 68 ms, Memory: 35552 KB -BaekjoonHub

--- 백준/Silver/6566. 애너그램 그룹/애너그램 그룹.py
+++ 백준/Silver/6566. 애너그램 그룹/애너그램 그룹.py
@@ -1,13 +1,8 @@
-dict = {}
-for line in [*open(0)]:
-    data = line.rstrip()
-    s_data = "".join(sorted(data))
-    if s_data not in dict:
-        dict[s_data] = [data]
-    else:
-        dict[s_data].append(data)
-key_list = sorted(dict.keys(), key=lambda x: (-len(dict[x]), dict[x]))
-for i in range(5):
-    new_list = sorted(set(dict[key_list[i]]))
-    joined = " ".join(new_list)
-    print(f"Group of size {len(dict[key_list[i]])}: {joined} .")
+D = {}
+for d in [*open(0)]:
+    l = d.rstrip()
+    s = "".join(sorted(l))
+    if s not in D:D[s]=[l]
+    else:D[s]+=[l]
+for i in sorted(D.keys(), key=lambda x: (-len(D[x]), D[x]))[:5]:
+    print(f"Group of size {len(D[i])}: {' '.join(sorted(set(D[i])))} .")
