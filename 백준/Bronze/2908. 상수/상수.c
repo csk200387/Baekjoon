cc747dd5db30f362567c9f96f834e9d7041c2376
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-08-29 10:17:14 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.c
+++ 백준/Bronze/2908. 상수/상수.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
-int main(void)
-{
+int main(void) {
     char a[4];
     char b[4];
     int i, t;
     scanf("%s %s", a, b);
-
-
-    for (t = 2; t >= 0; t--)
-    {
-        if (a[t] > b[t])
-        {
+    for (t = 2; t >= 0; t--) {
+        if (a[t] > b[t]) {
             for (i = 2; i >= 0; i--)
-            {
                 printf("%c", a[i]);
-            }
             break;
         }
         else if (b[t] > a[t])
         {
             for (i = 2; i >= 0; i--)
-            {
                 printf("%c", b[i]);
-            }
             break;
         }
     }
