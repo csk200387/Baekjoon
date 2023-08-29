9487d1669a4fcebb5054c18b860f699e39b67bbb
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-08-29 10:16:09 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.c
+++ 백준/Bronze/2908. 상수/상수.c
@@ -1,67 +1,30 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
 int main(void)
 {
     char a[4];
     char b[4];
-    int i;
+    int i, t;
     scanf("%s %s", a, b);
 
 
-
-    if (a[2] == b[2])
+    for (t = 2; t >= 0; t--)
     {
-        if (a[1] == b[1])
-        {
-            if (a[0] == b[0])
-            {
-                for (i = 2; i >= 0; i--)
-                {
-                    printf("%c", a[i]);
-                }
-            } 
-            else if (a[0] > b[0])
-            {
-                for (i = 2; i >= 0; i--)
-                {
-                    printf("%c", a[i]);
-                }
-            }
-            else if (b[0] > a[0])
-            {
-                for (i = 2; i >= 0; i--)
-                {
-                    printf("%c", b[i]);
-                }
-            }
-        }
-        else if(a[1]>b[1])
+        if (a[t] > b[t])
         {
             for (i = 2; i >= 0; i--)
             {
                 printf("%c", a[i]);
             }
+            break;
         }
-        else if (b[1] > a[1])
+        else if (b[t] > a[t])
         {
             for (i = 2; i >= 0; i--)
             {
                 printf("%c", b[i]);
             }
+            break;
         }
     }
-    else if (a[2] > b[2])
-    {
-        for (i = 2; i >= 0; i--)
-        {
-            printf("%c", a[i]);
-        }
-    }
-    else if (b[2] > a[2])
-        for (i = 2; i >= 0; i--)
-        {
-            printf("%c", b[i]);
-        }
+    return 0;
 }
