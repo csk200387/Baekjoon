7da4b70aaa35cfab7c6fd8eaceac5868e8f37941
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-10-11 18:09:11 +0900

    [Bronze I] Title: 최소공배수, Time: 128 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,7 +1,4 @@
 import math
-num = int(input())
-for i in range(num):
-    inp = input().split()
-    a = int(inp[0])
-    b = int(inp[1])
+for i in range(int(input())):
+    a,b = map(int,input().split())
     print(int(a * b / math.gcd(a, b)))
