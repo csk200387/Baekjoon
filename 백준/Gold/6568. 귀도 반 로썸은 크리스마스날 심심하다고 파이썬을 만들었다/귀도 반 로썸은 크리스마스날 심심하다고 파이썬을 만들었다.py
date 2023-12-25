03b6ea3061e32d0ce1e10c087cd02683a5b0fc21
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-12-25 20:09:06 +0900

    [Gold V] Title: 귀도 반 로썸은 크리스마스날 심심하다고 파이썬을 만들었다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/6568. 귀도 반 로썸은 크리스마스날 심심하다고 파이썬을 만들었다/귀도 반 로썸은 크리스마스날 심심하다고 파이썬을 만들었다.py
+++ 백준/Gold/6568. 귀도 반 로썸은 크리스마스날 심심하다고 파이썬을 만들었다/귀도 반 로썸은 크리스마스날 심심하다고 파이썬을 만들었다.py
@@ -1,42 +1,23 @@
 import sys
-
 while True:
-    try:
-        mem = [int(sys.stdin.readline().rstrip(), 2) for _ in range(32)]
-
-        adder = 0
-        pc = 0
-        while True:
-            cmd = mem[pc] // (2**5)
-            value = mem[pc] % (2**5)
-            pc += 1
-            pc %= 32
-            match cmd:
-                case 0:
-                    mem[value] = adder
-
-                case 1:
-                    adder = mem[value]
-
-                case 2:
-                    if adder == 0b0:
-                        pc = value
-
-                case 3:
-                    pass
-
-                case 4:
-                    adder = (adder + 255) % (2**8)
-
-                case 5:
-                    adder = (adder + 1) % (2**8)
-
-                case 6:
-                    pc = value
-
-                case 7:
-                    break
-                
-        print(f"{adder:08b}")
-    except:
-        break
+ try:
+  m=[int(sys.stdin.readline(),2)for _ in range(32)]
+  a=0
+  p=0
+  while True:
+   c=m[p]//2**5
+   v=m[p]%2**5
+   p+=1
+   p%=32
+   match c:
+    case 0:m[v]=a
+    case 1:a=m[v]
+    case 2:
+     if not a:p=v
+    case 3:pass
+    case 4:a=(a+255)%2**8
+    case 5:a=(a+1)%2**8
+    case 6:p =v
+    case 7:break
+  print(f"{a:08b}")
+ except:break
