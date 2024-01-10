af2296ea1630d01a34a001c6118abe6a39deb1a8
csk200387 <102679480+csk200387@users.noreply.github.com>
2024-01-10 15:55:39 +0900

    [Silver V] Title: 폴리오미노, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,12 +1,4 @@
 d = input()
-arr = []
-for i in d.split("."):
-    l = len(i)
-    if i == '':arr.append(i)
-    else:
-        if l % 2 == 1:exit(print(-1))
-        elif l % 4 == 0:arr.append("A"*l)
-        elif l % 2 == 0:
-            if l > 2:arr.append("A"*4*(l//4)+"B"*(l%4))
-            else:arr.append("B"*l)
-print(*arr, sep=".")
+while "XXXX" in d:d=d.replace("XXXX", "AAAA")
+while "XX" in d:d=d.replace("XX", "BB")
+print(-1 if "X" in d else d)
