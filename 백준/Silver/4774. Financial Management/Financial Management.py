371faa4ac61dca0c3db627895262dec45b09fbab
csk200387 <102679480+csk200387@users.noreply.github.com>
2024-02-07 00:07:00 +0900

    [Silver III] Title: Financial Management, Time: 72 ms, Memory: 36476 KB -BaekjoonHub

--- 백준/Silver/4774. Financial Management/Financial Management.py
+++ 백준/Silver/4774. Financial Management/Financial Management.py
@@ -1,8 +1,3 @@
-from decimal import *
-import sys
-input = lambda:sys.stdin.readline().rstrip()
-getcontext().rounding = ROUND_HALF_UP
-s = 0
-for i in range(12):
-    s += Decimal(input())
-print("$", round(s/12, 2), sep="")
+from decimal import*
+getcontext().rounding=ROUND_HALF_UP
+print(f"${sum(map(lambda x:Decimal(x),open(0)))/12:.2f}")
