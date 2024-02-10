deff1f9270999568036349f795e70985c3919c00
csk200387 <102679480+csk200387@users.noreply.github.com>
2024-02-10 22:23:26 +0900

    [Silver V] Title: 이장님 초대, Time: 88 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/9237. 이장님 초대/이장님 초대.py
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.py
@@ -1,3 +1,2 @@
-n=int(input())
-arr=sorted(map(int,input().split()),reverse=1)
-print(sum(max(enumerate(arr,1),key=sum))+1)
+input()
+print(sum(max(enumerate(sorted(map(int,input().split()),reverse=1),1),key=sum))+1)
