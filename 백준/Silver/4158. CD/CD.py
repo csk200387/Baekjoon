abe0bb365e681518e8e29b5022734d16f258f45a
csk200387 <102679480+csk200387@users.noreply.github.com>
2024-02-22 16:28:55 +0900

    [Silver V] Title: CD, Time: 2160 ms, Memory: 287640 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.py
+++ 백준/Silver/4158. CD/CD.py
@@ -1,8 +1,6 @@
 import sys
-input = lambda:sys.stdin.readline().rstrip()
-while True:
-    n, m = map(int, input().split())
-    if n == m == 0:break
-    N = {input() for i in range(n)}
-    M = {input() for i in range(m)}
-    print(len(N & M))
+c=sys.stdin.readline
+while 1:
+ n,m=map(int,input().split())
+ if n==m==0:break
+ print(len({c()for i in range(n)}&{c()for i in range(m)}))
