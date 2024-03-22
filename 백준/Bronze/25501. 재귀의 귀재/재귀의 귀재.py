a27edc7900aafaa5620d84fc87c6b3ffb04c4cad
csk200387 <102679480+csk200387@users.noreply.github.com>
2024-03-22 20:15:22 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 140 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -1,18 +1,13 @@
 import sys
 input = lambda:sys.stdin.readline().rstrip()
-sys.setrecursionlimit(10**6)
-
-def is_palindrome(s, start, end, count=0):
-    count += 1
-    if start >= end:
-        return 1, count    
-    if s[start] == s[end]:
-        return is_palindrome(s, start + 1, end - 1, count)
-    else:
-        return 0, count
-
 n = int(input())
 for i in range(n):
     data = input()
-    result, count = is_palindrome(data, 0, len(data) - 1)
-    print(result, count)
+    isp = True
+    cnt = 1
+    for j in range(len(data)//2):
+        if data[j] != data[-1-j]:
+            isp = False
+            break
+        cnt += 1
+    print(int(isp), cnt)
