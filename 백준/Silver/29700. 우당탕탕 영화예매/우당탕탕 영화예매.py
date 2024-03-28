c2ea41ac8e92b6cc76bc9ce3d461c47f99086fc8
csk200387 <102679480+csk200387@users.noreply.github.com>
2024-03-28 15:33:18 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 568 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.py
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.py
@@ -1,13 +1,11 @@
 import sys
+input = lambda:sys.stdin.readline().rstrip()
 n, m, k = map(int, input().split())
-arr = [input() for i in range(n)]
 cnt = 0
 t = "0" * k
 for i in range(n):
-    start, end = 0, k
-    while end <= m:
-        if arr[i][start:end] == t:
-            cnt += 1
-        start += 1
-        end += 1
+    d = input().split('1')
+    for i in d:
+        if len(i) >= k:
+            cnt += len(i) - k + 1
 print(cnt)
